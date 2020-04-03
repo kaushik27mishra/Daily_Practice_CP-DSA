9dfe0c828bd3b4beeb3286586be25ddacf3b44b5
Kaushik Mishra <kmishra27.km@gmail.com>
2020-04-04 02:05:15 +0530

    peakfinding alternate recurrsion

--- MIT6.006/peakfinding_array.cpp
+++ MIT6.006/peakfinding_array.cpp
@@ -18,12 +18,17 @@ void write(T&&... args) {
 int main() {
     int n;
     cin>>n;
-    int low=0,high=n;
+    int low=0,high=n-1;
     vector<int> arr(n,0);
     int i; 
 
     fo(i,n)
-        read(arr[i]);
+        cin>>arr[n];
+
+    if(arr[0] > arr[1]) 
+      return 0; 
+    if(arr[n-1] > arr[n-2]) 
+      return n-1; 
 
     while(true){
 
@@ -44,3 +49,39 @@ int main() {
 
     return 0;
 }
+
+
+// Alternate solution
+
+// #include <bits/stdc++.h> 
+// using namespace std; 
+// 
+// int findPeakUtil(int arr[], int low, 
+				// int high, int n) 
+// { 
+	// int mid = low + (high - low)/2; /* (low + high)/2 */
+// 
+	// if ((mid == 0 || arr[mid - 1] <= arr[mid]) && 
+		// (mid == n - 1 || arr[mid + 1] <= arr[mid])) 
+		// return mid; 
+// 
+	// else if (mid > 0 && arr[mid - 1] > arr[mid]) 
+		// return findPeakUtil(arr, low, (mid - 1), n); 
+// 
+	// else return findPeakUtil(arr, (mid + 1), high, n); 
+// } 
+// 
+// int findPeak(int arr[], int n) 
+// { 
+	// return findPeakUtil(arr, 0, n - 1, n); 
+// } 
+// 
+// int main() 
+// { 
+	// int arr[] = {1, 3, 20, 4, 1, 0}; 
+	// int n = sizeof(arr) / sizeof(arr[0]); 
+	// cout << "Index of a peak point is "
+		// << findPeak(arr, n); 
+	// return 0; 
+// } 
+
