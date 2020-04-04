f8ed019065c8c528db5ef93ed65b4f23622231df
Kaushik Mishra <kmishra27.km@gmail.com>
2020-04-04 18:01:25 +0530

    template added

---
+++ MIT6.006/peakfinding_multiarray.cpp
@@ -0,0 +1,24 @@
+//greedy ascent algotrithm
+#include <bits/stdc++.h>
+using namespace std;
+
+#define deb(x) cout<<#x<<" "<<x<<endl;
+#define fo(i,n) for(i=0;i<n;i++)
+#define Fo(i,k,n) for(i=k;i<n;i++)
+
+template<typename... T>
+void read(T&... args) {
+    ((cin>>args), ...);
+}
+
+template<typename... T>
+void write(T&&... args) {
+    ((cout<<args<<" "), ...);
+}
+
+int main() {
+
+    return 0;
+}
+
+
