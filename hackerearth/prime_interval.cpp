ec973514671deac1719574f42b5401a73e25fe7f
Kaushik Mishra <kmishra27.km@gmail.com>
2020-04-12 15:29:56 +0530

    prime interval hackerearth

--- hackerearth/prime_interval.cpp
+++ hackerearth/prime_interval.cpp
@@ -3,23 +3,27 @@ using namespace std;
 
 #define deb(x) cout<<#x<<" "<<x<<endl;
 #define fo(i,n) for(i=0;i<n;i++)
-#define Fo(i,k,n) for(i=k;i<n;i++)
+#define Fo(i,k,n) for(i=k;i<=n;i++)
 
-template<typename... T>
-void read(T&... args) {
-    ((cin>>args), ...);
-}
+bool isPrime(int n) {
 
-template<typename... T>
-void write(T&&... args) {
-    ((cout<<args<<" "), ...);
+    if(n==1)
+        return false;
+
+    for(int i=2;i*i<=n;i++) {
+        if(n%i==0)
+            return false;
+    }
+
+    return true;
 }
 
 int main() {
-    int T;
-    cin>>T;
-    while(T--) {
-        
+    int L,R,i(0);
+    cin>>L>>R;
+    Fo(i,L,R) {
+        if(isPrime(i))
+            cout<<i<<" ";
     }
     return 0;
 }
