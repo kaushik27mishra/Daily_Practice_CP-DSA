2f7056603d8b4f2ff3c0d31a15a960c320b03735
Kaushik Mishra <kmishra27.km@gmail.com>
2020-04-27 01:21:54 +0530

    finding factorial of small number SPOJ

--- SPOJ/small_factorials.cpp
+++ SPOJ/small_factorials.cpp
@@ -1,23 +1,55 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include<iostream> 
+using namespace std; 
 
-#define deb(x) cout<<#x<<" "<<x<<endl;
-#define fo(i,n) for(int i=0;i<n;i++)
-#define Fo(i,k,n) for(int i=k;i<n;i++)
-#define N 101
-#define ll long long
-#define endl "\n"
-#define mod 1000000007
-#define pb push_back
-#define F first
-#define S second
+#define MAX 500 
 
-int main() {
-    int T;
-    cin>>T;
-    while(T--) {
+int multiply(int x, int res[], int res_size); 
 
+void factorial(int n) 
+{ 
+	int res[MAX]; 
+
+	res[0] = 1; 
+	int res_size = 1; 
+
+	for (int x=2; x<=n; x++) 
+		res_size = multiply(x, res, res_size); 
+
+	for (int i=res_size-1; i>=0; i--) 
+		cout << res[i]; 
+    cout<<endl; 
+} 
+
+int multiply(int x, int res[], int res_size) 
+{ 
+	int carry = 0; 
+
+	for (int i=0; i<res_size; i++) 
+	{ 
+		int prod = res[i] * x + carry; 
+
+		res[i] = prod % 10; 
+
+		carry = prod/10;	 
+	} 
+
+	while (carry) 
+	{ 
+		res[res_size] = carry%10; 
+		carry = carry/10; 
+		res_size++; 
+	} 
+	return res_size; 
+} 
+
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--) {
+        int n;
+        cin>>n;
+	    factorial(n);
     }
-
-    return 0;
-}
+	return 0; 
+} 
