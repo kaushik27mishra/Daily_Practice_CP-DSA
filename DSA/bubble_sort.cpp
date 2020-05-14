8f865ac415f70f1b4c3c7acb1800c952d6b7886c
Kaushik Mishra <kmishra27.km@gmail.com>
2020-05-14 17:07:19 +0530

    bubble sort

---
+++ DSA/bubble_sort.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void bubbleSort(int arr[],int n) {
+
+    int j,i;
+    bool flag=false;
+    for(i=0;i<n;i++) {
+        flag=false;
+        for(j=0;j<n-i-1;j++) {
+            if(arr[j]<arr[j-1]) 
+                flag=true, swap(arr[j],arr[j-1]);
+        }
+
+        if(!flag) break;
+    }
+
+}
+
+int main() {
+
+    int arr[10];
+    for(int i=0;i<10;i++) {
+        cin>>arr[i];
+    }
+
+    bubbleSort(arr,10);
+
+    for(int i=0;i<10;i++) {
+        cout<<arr[i]<<" ";
+    }
+
+    return 0;
+}
