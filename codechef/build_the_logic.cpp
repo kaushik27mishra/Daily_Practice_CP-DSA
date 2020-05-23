cf729e76a75703422f408578e28ac5778f0bc162
Kaushik Mishra <kmishra27.km@gmail.com>
2020-05-23 14:07:28 +0530

    build the logic codechef

---
+++ codechef/build_the_logic.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+ll pattern(int n) {
+    if(n&1) {
+        return (ll)n*(n+1)/2;
+    }
+    else {
+        return (ll)n*(n)/2;
+    }
+}
+int main() {
+    int T,n;
+    cin>>T;
+    while(T--) {
+        cin>>n;
+        cout<<pattern(n)<<endl;
+    }
+
+    return 0;
+}
