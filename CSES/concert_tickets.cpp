16738a4ab692eb3113b43e29c803e016bba68366
Kaushik Mishra <kmishra27.km@gmail.com>
2020-05-31 19:50:32 +0530

    concert tickets CSES

---
+++ CSES/concert_tickets.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define deb(x) cout<<#x<<" "<<x<<endl;
+#define fo(i,n) for(int i=0;i<n;i++)
+#define Fo(i,k,n) for(int i=k;i<n;i++)
+#define N 101
+#define ll long long
+#define endl "\n"
+#define mod 1000000007
+#define pb push_back
+#define F first
+#define S second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+typedef vector<int> vi;
+
+int main() {
+
+    int n,m,x;
+    cin>>n>>m;
+    multiset<int,greater<int>> tickets;
+    vi customers(m);
+    for(int i=0;i<n;i++) {
+        cin>>x;
+        tickets.insert(x);
+    }
+
+    for(int i=0;i<m;i++) {
+        cin>>customers[i];
+        auto itr=tickets.lower_bound(customers[i]);
+        if(itr==tickets.end()) {
+            cout<<"-1\n"<<endl;
+        }
+        else {
+            cout<<*itr<<endl;
+            tickets.erase(itr);
+        }            
+    }    
+    
+    return 0;
+}
