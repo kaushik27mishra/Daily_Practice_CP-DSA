420e3a22817c6a468faf1283e133881179d57003
Kaushik Mishra <kmishra27.km@gmail.com>
2020-06-25 18:35:29 +0530

    0 1 knapsack with memorization

--- DynamicProgramming/0_1_knapsack_problem.cpp
+++ DynamicProgramming/0_1_knapsack_problem.cpp
@@ -3,23 +3,30 @@ using namespace std;
 
 #define deb(x) cout<<#x<<" "<<x<<endl;
 
+int t[102][1002];
+
 int knapsack(int wt[],int val[],int w,int n) {
     if(n==0 || w==0) {
         return 0;
     }
 
+    if(t[n][w]!=-1) {
+        return t[n][w];
+    }
+
     if(wt[n-1]<=w) {
-        return max(val[n-1]+knapsack(wt,val,w-wt[n-1],n-1),knapsack(wt,val,w,n-1));
+        return t[n][w]=max(val[n-1]+knapsack(wt,val,w-wt[n-1],n-1),knapsack(wt,val,w,n-1));
     }
     else {
-        return knapsack(wt,val,w,n-1);
+        return t[n][w]=knapsack(wt,val,w,n-1);
     } 
 }
 
 int main() {
     int val[] = { 60, 100, 120 }; 
     int wt[] = { 10, 20, 30 }; 
-    int W = 50; 
+    int W = 50;
+    memset(t,-1,sizeof(t)); 
     cout << knapsack(wt, val,W, 3)<<endl;  
     return 0;
 }
