542fcb33fef837b9a761f34128e9a88a85d02170
Kaushik Mishra <kmishra27.km@gmail.com>
2020-06-25 12:50:35 +0530

    GCD compression codeforces

--- codeforces/gcd_compression.cpp
+++ codeforces/gcd_compression.cpp
@@ -1,29 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-#define deb(x) cout<<#x<<" "<<x<<endl;
-#define fo(i,n) for(int i=0;i<n;i++)
-#define Fo(i,k,n) for(int i=k;i<n;i++)
-#define N 101
-#define ll long long
+ 
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define endl "\n"
-#define mod 1000000007
-#define pb push_back
-#define F first
-#define S second
-#define mp make_pair
-#define lb lower_bound
-#define ub upper_bound
-#define all(x) x.begin(), x.end()
-typedef vector<int> vi;
+#define int long long
 
-int main() {
-    
-    int T;
-    cin>>T;
-    while(T--) {
+const int N = 2e5 + 5;
 
-    }
+int n;
+int a[N];
 
-    return 0;
+int main()
+{
+	int t;
+	cin >> t;
+	while(t--)
+	{
+		cin >> n;
+		vector< int > even, odd;
+		for(int i = 1; i <= 2 * n; i++)
+		{
+			cin >> a[i];
+			if(a[i] % 2)
+				odd.push_back(i);
+			else
+				even.push_back(i);
+		}
+		vector< pair< int, int > > ans;
+		for(int i = 0; i + 1 < odd.size(); i += 2)
+			ans.push_back({odd[i], odd[i + 1]});
+		for(int i = 0; i + 1 < even.size(); i += 2)
+			ans.push_back({even[i], even[i + 1]});
+		for(int i = 0; i < n - 1; i++)
+			cout << ans[i].first << " " << ans[i].second << endl;
+	}
+	return 0;
 }
