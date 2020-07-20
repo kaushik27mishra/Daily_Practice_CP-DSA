c6b6cfe4f8ae1bed90e98025924e9920f0353fc3
Kaushik Mishra <kmishra27.km@gmail.com>
2020-07-20 12:36:46 +0530

    partition equal subset sum Dynamic programming

--- DynamicProgramming/partition_equal_subset_sum.cpp
+++ DynamicProgramming/partition_equal_subset_sum.cpp
@@ -6,6 +6,48 @@ public:
         for(int i=0;i<n;i++) {
             sum=sum+(long long)nums[i];
         }
+        if(sum%2!=0)
+            return false;
+        
+        int subsetSum=sum/2;
+        
+        return solve(nums,subsetSum,n);
+    }
+    
+    bool solve(vector<int> nums,long long subsetSum,int n) {
+        bool t[n+1][subsetSum+1];
+        
+        for(int i=0;i<=n;i++) {
+            t[i][0]=true;
+        }
+        
+        for(int i=1;i<=subsetSum;i++) {
+            t[0][i]=false;
+        }
+        
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=subsetSum;j++) {
+                if(j<nums[i-1])
+                    t[i][j]=t[i-1][j];
+                else
+                    t[i][j]=t[i-1][j-nums[i-1]] || t[i-1][j];
+            }
+        }
+        
+        return t[n][subsetSum];
+    }
+};
+
+/*
+recurrsive solution
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        long long sum=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++) {
+            sum=sum+(long long)nums[i];
+        }
         int subsetSum=sum/2;
         
         return solve(nums,subsetSum,n);
@@ -24,3 +66,4 @@ public:
         }
     }
 };
+*/
