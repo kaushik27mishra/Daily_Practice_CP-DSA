fe4425aef4da16ab91d782a086e089469a9daabf
Kaushik Mishra <kmishra27.km@gmail.com>
2020-07-20 13:44:14 +0530

    count of subsets sum dynamic programming

--- DynamicProgramming/count_of_subsets_sum.cpp
+++ DynamicProgramming/count_of_subsets_sum.cpp
@@ -2,6 +2,31 @@
 using namespace std;
 
 int solve(int arr[],int k,int n) {
+    int t[n+1][k+1];
+    for(int i=0;i<=n;i++)
+        t[i][0]=1;
+    for(int j=1;j<=k;j++)
+        t[0][j]=0;
+
+    for(int i=1;i<=n;i++) {
+        for(int j=1;j<=k;j++) {
+            if(arr[i-1]>j)
+                t[i][j]=t[i-1][j];
+            else
+                t[i][j]=t[i-1][j]+t[i-1][j-arr[i-1]];
+        }
+    }
+    return t[n][k];
+}
+
+
+int main() {
+    int arr[]={1, 2, 3, 3};
+    int k=6;
+    cout<<solve(arr,6,4)<<endl;
+}
+/*
+int solve(int arr[],int k,int n) {
     if(k==0)
         return 1;
     if(k!=0 && n==0)
@@ -12,9 +37,4 @@ int solve(int arr[],int k,int n) {
     else
         return solve(arr,k-arr[n-1],n-1) + solve(arr,k,n-1); 
 }
-
-int main() {
-    int arr[]={1, 2, 3, 3};
-    int k=6;
-    cout<<solve(arr,6,4)<<endl;
-}
+*/
