5bb65d627ce16c7dd5523c9df50c89549e55c4e3
Kaushik Mishra <kmishra27.km@gmail.com>
2020-07-20 11:25:34 +0530

    subsetsum using Dynamic Programming

--- DynamicProgramming/subset_sum.cpp
+++ DynamicProgramming/subset_sum.cpp
@@ -3,15 +3,26 @@ using namespace std;
 
 bool isSubsetSum(vector<int> set, int n, int sum) 
 { 
-    if (sum == 0) 
-        return true; 
-    if (n == 0 && sum != 0) 
-        return false; 
-  
-    if (set[n - 1] > sum) 
-        return isSubsetSum(set, n - 1, sum); 
-  
-    return isSubsetSum(set, n - 1, sum) || isSubsetSum(set, n - 1, sum - set[n - 1]); 
+    bool t[n+1][sum+1];
+    for(int i=0;i<n+1;i++) {
+        t[i][0]=true;
+    }
+
+    for(int i=1;i<sum+1;i++) {
+        t[0][i]=false;
+    }
+
+    for(int i=1;i<n+1;i++) {
+        for(int j=0;j<sum+1;j++) {
+            if(set[i-1]>j) {
+                t[i][j]=t[i-1][j];
+            }
+            else {
+                t[i][j]=t[i-1][j] || t[i-1][j-set[i-1]];
+            }
+        }
+    }
+    return t[n][sum];
 } 
 
 int main() {
