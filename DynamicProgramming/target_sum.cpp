416f04b4bddc16146cd173086872bc749385bbfe
Kaushik Mishra <kmishra27.km@gmail.com>
2020-07-21 23:13:26 +0530

    target sum Dynamic Programming

---
+++ DynamicProgramming/target_sum.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+int solve(int arr[],int n,int subsetSum) {
+    int t[n+1][subsetSum+1];
+    for(int i=0;i<=n;i++)
+        t[i][0]=1;
+
+    for(int i=1;i<=subsetSum;i++)
+        t[0][i]=0; 
+
+    for(int i=1;i<=n;i++) {
+        for(int j=1;j<=subsetSum;j++) {
+            if(arr[i-1]>j) {
+                t[i][j]=t[i-1][j];
+            }
+            else {
+                t[i][j]=t[i-1][j]+t[i-1][j-arr[i-1]];
+            }
+        }
+    }
+    return t[n][subsetSum];
+}
+
+int main() {
+    int n=4;
+    int arr[]={1,1,2,3};
+    int sum=0;
+    for(int i=0;i<n;i++)
+        sum+=arr[i];
+    int target=1;
+    cout<<solve(arr,n,sum-target)<<endl;
+}
