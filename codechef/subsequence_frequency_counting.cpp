f09b1faea8028a50cc85a5985e1d58915c2b7142
Kaushik Mishra <kmishra27.km@gmail.com>
2020-08-17 13:20:08 +0530

    subsequence frequency counting subtask 2 solved codechef

--- codechef/subsequence_frequency_counting.cpp
+++ codechef/subsequence_frequency_counting.cpp
@@ -17,27 +17,40 @@ using namespace std;
 #define all(x) x.begin(), x.end()
 typedef vector<int> vi;
 
-ll C[5003][5003] = {0};
-ll P[1000] = {0};
+ll C[10004][10004] = {0};
+ll P[500002] = {0};
 
-ll binomialCoeffUtil(int n, int k) 
-{ 
-    ll dp[n+1][k+1]={0};
-    int i, j; 
+ll binomialCoeffUtil(int n, int k)  
+{  
+    ll res = 1;  
   
-    for (i = 0; i <= n; i++) 
-    { 
-        for (j = 0; j <= min(i, k); j++) 
-        { 
-            if (j == 0 || j == i) 
-                dp[i][j] = 1; 
-            else
-                dp[i][j] = (dp[i - 1][j - 1]+dp[i - 1][j])%mod;
-        }
-    } 
-    return dp[n][k];
+    if ( k > n - k )  
+        k = n - k;  
+  
+    for (int i = 0; i < k; ++i)  
+    {  
+        res = (res* (n - i))%mod;  
+        res /= (i + 1);  
+    }  
+  
+    return res;  
 }
 
+ll binaryExpoUtil(ll a,ll n) {
+    if (n==0) return 1;
+
+    ll res;
+    if(n&1) {
+        res=a%mod;
+        res=(res*binaryExpoUtil(a,n-1))%mod;
+    }
+    else {
+        res = binaryExpoUtil(a,n/2)%mod;
+        res=(res*res)%mod;
+    }
+
+    return (res)%mod;
+}
 
 void binomialCoeff(int n, int k) 
 { 
@@ -64,71 +77,95 @@ void power(int n) {
 }
 
 int main() {
-    binomialCoeff(5002,5002);
+    binomialCoeff(10003,10003);
+    power(500000);
     int T;
     cin>>T;
     while(T--) {
         int n;
         cin>>n;
-        power(n);
         vector<int> arr(n,0);
         map<int,int> count;
         for(int i=0;i<n;i++)
             cin>>arr[i], count[arr[i]]++;
-        
-        map<int,int> op;
+
+        unordered_map<int,vector<ll>> nCr_sum;
+        bool flag_count=true;
         for(auto it : count) {
-
-            int number=it.first;
-            ll sum=0;
-
+            if(it.second>1)
+                flag_count=false;
+            nCr_sum[it.first].resize(it.second+1);
+            nCr_sum[it.first][0]=1;
             for(int i=1;i<=it.second;i++) {
+                if(it.second<=10003 && i<=10003)
+                    nCr_sum[it.first][i]=(nCr_sum[it.first][i-1]+C[it.second][i])%mod;
+                else
+                    nCr_sum[it.first][i]=(nCr_sum[it.first][i-1]+binomialCoeffUtil(it.second,i))%mod;
+            }
+        }
 
-                ll temp=1;
+        unordered_map<int,int> op;
+        if(flag_count) {
+            int i=1;
+            for(auto it : count) {
+                op[it.first]=P[n-i];
+                i++;
+            }
+        }
+        else {
+            for(auto it : count) {
 
-                for(auto aux : count) {
+                int number=it.first;
+                ll sum=0;
 
-                    ll choose=0;
-                    if(aux.first<number) {
-                        if(aux.second<i) {
-                            choose=P[aux.second];
+                for(int i=1;i<=it.second;i++) {
+
+                    ll temp=1;
+
+                    for(auto aux : count) {
+
+                        ll choose=0;
+                        if(aux.first<number) {
+                            if(aux.second<i) {
+                                if(aux.second<=500000)
+                                    choose = P[aux.second];
+                                else
+                                    choose = binaryExpoUtil(2,aux.second);
+                            }
+                            else {
+                                choose = nCr_sum[aux.first][i-1];
+                            }
+
+                        }
+                        else if(aux.first>number) {
+                            if(aux.second<=i) {
+                                if(aux.second<=500000)
+                                    choose = P[aux.second];
+                                else
+                                    choose = binaryExpoUtil(2,aux.second);
+                            }
+                            else {
+                                choose = nCr_sum[aux.first][i];
+                            }
+
                         }
                         else {
-                            for(int j=0;j<i && j<=aux.second;j++) {
-                                if(aux.second<5000 && j<5000)
-                                    choose=(choose+C[aux.second][j])%mod;
-                                else
-                                    choose=(choose+binomialCoeffUtil(aux.second,j))%mod;
-                            }
+                            if(aux.second<10003 && i<10003)
+                                choose=(C[aux.second][i])%mod;
+                            else
+                                choose=(binomialCoeffUtil(aux.second,i))%mod;
                         }
 
-                    }
-                    else if(aux.first>number) {
-                        if(aux.second<=i) {
-                            choose=P[aux.second];
-                        }
-                        else {
-                            for(int j=0;j<=i && j<=aux.second;j++) {
-                                if(aux.second<5000 && j<5000)
-                                    choose=(choose+C[aux.second][j])%mod;
-                                else
-                                    choose=(choose+binomialCoeffUtil(aux.second,j))%mod;
-                            }
-                        }
-
-                    }
-                    else {
-                        choose=C[aux.second][i];
+                        temp=(temp*choose)%mod;
                     }
 
-                    temp=(temp*choose)%mod;
+                    sum=(sum+temp)%mod;
                 }
 
-                sum=(sum+temp)%mod;
-            }
-
-            op[number]=sum;
+                op[number]=sum;
+            }   
         }
+        
 
         for(int i=1;i<=n;i++) {
             cout<<op[i]<<" ";
