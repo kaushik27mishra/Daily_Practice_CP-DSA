4a879f58e4a6930f20367edaea993e21a3873285
Kaushik Mishra <kmishra27.km@gmail.com>
2020-10-06 14:50:45 +0530

    power of 2 gfg

--- geeksforgeeks/power_of_two.cpp
+++ geeksforgeeks/power_of_two.cpp
@@ -17,24 +17,13 @@ using namespace std;
 #define all(x) x.begin(), x.end()
 typedef vector<int> vi;
 
-int table[256]; // 256 because 8 bits count upto 256
-
-void initialize() {
-    table[0] = 0;
-    for (int i = 0; i < 256; i++)
-    {
-        table[i] = (i & 1) + table[i / 2];
-    }
-}
-
-int count(int n) {
-	return table[n & 0xff] + table[(n >> 8) & 0xff] + table[(n >> 16) & 0xff] + table[n >> 24];
-}
-
 int main() {
-    initialize();
     int n;
     cin>>n;
-    cout<<count(n)<<endl;
+    if((n&(n-1))==0) 
+        cout<<"YES"<<endl;
+    else 
+        cout<<"NO"<<endl; 
+
     return 0;
 }
