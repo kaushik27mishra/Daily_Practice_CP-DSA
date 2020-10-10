b35bcb29620553b9902442999704b081ad8231a8
Kaushik Mishra <kmishra27.km@gmail.com>
2020-10-10 18:16:04 +0530

    longest consecutive 1s gfg

---
+++ geeksforgeeks/longest_consecutive_1s.cpp
@@ -0,0 +1,43 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+//User function Template for C++
+
+/*  Function to calculate the largest consecutive ones
+*   x: given input to calculate the largest consecutive ones
+*/
+int maxConsecutiveOnes(int x)
+{
+    int res=0;
+    while(x>0) {
+        res++;
+        x = x & (x>>1);
+    }
+    return res;
+}
+
+
+
+// { Driver Code Starts.
+
+// Driver Code
+int main() {
+	int t;
+	cin>>t;//testcases
+	while(t--)
+	{
+		int n;
+		cin>>n;//input n
+		
+		//calling maxConsecutiveOnes() function
+		cout<<maxConsecutiveOnes(n)<<endl;
+	}
+	return 0;
+}  // } Driver Code Ends
