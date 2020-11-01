d5fba4d1b442f0f678cb347ab20583b8ed83ee50
Kaushik Mishra <kmishra27.km@gmail.com>
2020-11-01 23:50:17 +0530

    convert binary number in a linked list to integer

---
+++ leetcode/convert_binary_number_in_a_linked_list_to_integer.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int getDecimalValue(ListNode* head) {
+        int res=0;
+        while(head!=NULL) {
+            res*=2;
+            res+=head->val;
+            head=head->next;
+        }
+        return res;
+    }
+};
