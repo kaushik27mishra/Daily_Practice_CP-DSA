f79d5ff2101bf4da941ef2fae64bb8147848bb32
Kaushik Mishra <kmishra27.km@gmail.com>
2020-11-01 23:52:56 +0530

    convert binary number in a linked list to integer using bitwise operator leetcode

--- leetcode/convert_binary_number_in_a_linked_list_to_integer.cpp
+++ leetcode/convert_binary_number_in_a_linked_list_to_integer.cpp
@@ -20,3 +20,16 @@ public:
         return res;
     }
 };
+
+/*
+class Solution {
+    public int getDecimalValue(ListNode head) {
+        int num = head.val;
+        while (head.next != null) {
+            num = (num << 1) | head.next.val;
+            head = head.next;    
+        }
+        return num;
+    }
+}
+*/
