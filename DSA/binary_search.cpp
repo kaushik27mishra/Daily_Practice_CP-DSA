40afeae1d1f19d86a9092ec4963a95e1d84c426f
Kaushik Mishra <kmishra27.km@gmail.com>
2020-11-09 14:22:53 +0530

    add two number 2 leetcode

--- DSA/binary_search.cpp
+++ DSA/binary_search.cpp
@@ -12,35 +12,38 @@ using namespace std;
 #define F first
 #define S second
 
-int binarySearch(long int arr[],int n, int a) {
-    int left=0,right=n-1,mid,result=-1;
-    while(left<=right) {
-        mid=left+(right-left)/2;
+int binarySearch(long int arr[],int n, int a,bool first) {
+    int left = 0,right = n-1,mid, res=-1;
+    while(left <= right) {
+        mid = left+(right-left)/2;
 
         if(arr[mid]==a) {
-            result=mid;
-            right=mid-1; //for finding first occurence
-            // left = mid +1; for finding last occurence
+            res = mid;
+            if(first) {
+                right = mid - 1; // for finding first occurence
+            }
+            else {
+                left = mid + 1; // for finding last occurence
+            }
         }
 
         if(arr[mid]>a) right=mid-1;
         else left=mid+1;
     }
 
-    return result;
+    return res;
 }
 
 int main() {
     int n,q;
-    cin>>n>>q;
+    cin>>n;
     long int arr[n],a;
     fo(i,n) {
         cin>>arr[i];
     }
-    while(q--) {
-        cin>>a;
-        cout<<binarySearch(arr,n,a)<<endl;
-    }
+    cin>>a;
+    cout<<binarySearch(arr,n,a,false)<<endl;
+    cout<<binarySearch(arr,n,a,true)<<endl;
 
     return 0;
 }
