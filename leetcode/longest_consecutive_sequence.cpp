e02767e711bfb60a74bd6697ee6fd2b118c9a59a
Kaushik Mishra <kmishra27.km@gmail.com>
2020-11-16 12:09:17 +0530

    longest consecutive sequence leetcode

--- leetcode/longest_consecutive_sequence.cpp
+++ leetcode/longest_consecutive_sequence.cpp
@@ -24,3 +24,38 @@ public:
         return res;
     }
 };
+
+/*
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        if(nums.size()==0)
+            return 0;
+        unordered_set<int> s;
+        for(int i=0;i<nums.size();i++) {
+            s.insert(nums[i]);
+        }
+        
+        int res = 1;
+        while(s.size()>1) {
+            int number = *s.begin();
+            int count = 1;
+            s.erase(number);
+            int smaller = number - 1; 
+            int bigger = number + 1;
+            while(s.count(bigger)>0) {
+                s.erase(bigger);
+                count++;
+                bigger++;
+            }
+            while(s.count(smaller)>0) {
+                s.erase(smaller);
+                count++;
+                smaller--;
+            }
+            res = max(res, count);
+        }
+        return res;
+    }
+};
+*/
