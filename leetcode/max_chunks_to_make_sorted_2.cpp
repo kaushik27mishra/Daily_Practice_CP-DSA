80e882f0905330832e3c15e1e15bcfd77ce194b4
Kaushik Mishra <kmishra27.km@gmail.com>
2020-11-17 12:07:31 +0530

    max chunks to make sorted 2 leetcode

--- leetcode/max_chunks_to_make_sorted_2.cpp
+++ leetcode/max_chunks_to_make_sorted_2.cpp
@@ -22,3 +22,19 @@ public:
         return res+1;
     }
 };
+
+/*
+better approach
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        stack<int>st;
+        for(int i=0;i<arr.size();i++) {
+            int curmax=st.empty() ? arr[i]: max(st.top(),arr[i]);
+            while( (!st.empty()) && (st.top()>arr[i]) ) st.pop();
+            st.push(curmax);
+        }
+        return st.size();
+    }
+};
+*/
